c5a6a5fb9ce15a6f54885bb8397773e9d04f7dc7
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-06-12 21:35:09 +0530

    init backend

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-export const mongoDBURL =
-  "mongodb+srv://admin:admin@bookstore-mern.lyfziok.mongodb.net/?retryWrites=true&w=majority&appName=BookStore-MERN";
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-import express, { request, response } from "express";
-import { PORT, mongoDBURL } from "./config.js";
+import express from "express";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+import dotenv from "dotenv";
 import booksRoute from "./routes/booksRoute.js";
 
+dotenv.config();
 const app = express();
 
 // Middleware for parsing request body
@@ -18,11 +18,11 @@ app.use("/books", booksRoute);
 
 // Connecting database using URL
 mongoose
-  .connect(mongoDBURL)
+  .connect(process.env.MONGO_DB)
   .then(() => {
     console.log("App connected to database");
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening to port: ${process.env.PORT}`);
     });
   })
   .catch((error) => {
